3aa7e62c47dc8deea6e54fbabf41f62ca7a83225
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-06-01 12:27:55 -0300

    added dataBase.js:zap:

---
+++ js/dataBase.js
@@ -0,0 +1,47 @@
+const dataBase = [
+  { 
+    id: 0,
+    image: 'image/foto.jpg',
+    title: 'Beautiful View',
+    price: 458.83,
+    type: 'shirt',
+    discount: 2,
+    amount: 0,
+  },
+  {
+    id: 1,
+    image: 'image/foto-1.jpg',
+    title: 'Back To You',
+    price: 1587.25,
+    type: 'hats',
+    discount: 3,
+    amount: 0,
+  },
+  {
+    id: 2,
+    image: 'image/foto.jpg',
+    title: 'Power Of Thought',
+    price: 648.84,
+    type: 'pants',
+    discount: 8,
+    amount: 0,
+  },
+  {
+    id: 3,
+    image: 'image/foto-1.jpg',
+    title: 'The Depth Of The Sea',
+    price: 549.58,
+    type: 'shorts',
+    discount: 5,
+    amount: 0,
+  },
+  {
+    id: 4,
+    image: 'image/foto.jpg',
+    title: 'The Sky Is Full',
+    price: 348.54,
+    type: 'shoes',
+    discount: 4,
+    amount: 0,
+  }
+]
