1b725144461b29eec9c7701c49988d93128def15
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-08-19 18:51:42 -0300

    updated productsUpdated:zap:

--- js/productsUpdate.js
+++ js/productsUpdate.js
@@ -3,27 +3,71 @@ const pants = document.getElementById('pants');
 const shoes = document.getElementById('shoes');
 const shorts = document.getElementById('short');
 const hats = document.getElementById('hat');
+const all = document.getElementById('all');
+
+const element = document.querySelectorAll('.product-single');
+
+all.addEventListener('click', () => {
+  element.forEach((event) => {
+    console.log(event.getAttribute('dataset'));
+    event.setAttribute('style', 'display: flex');
+  })
+})
 
 shirts.addEventListener('click', () => {
-  menu_products.innerHTML = '';
-  initStore(shirts.textContent);
+  element.forEach((event) => {
+    console.log(event.getAttribute('dataset'));
+    if(event.getAttribute('dataset') != 'shirt') {
+      console.log(event.getAttribute('dataset')+' == '+"shirt");
+      event.setAttribute('style', 'display: none');
+    } else {
+      event.setAttribute('style', 'display: flex');
+    }
+  })
 })
 pants.addEventListener('click', () => {
-  menu_products.innerHTML = '';
-  initStore(pants.textContent);
+  element.forEach((event) => {
+    console.log(event.getAttribute('dataset'));
+    if(event.getAttribute('dataset') != 'pants') {
+      console.log(event.getAttribute('dataset')+' == '+"pants");
+      event.setAttribute('style', 'display: none');
+    } else {
+      event.setAttribute('style', 'display: flex');
+    }
+  })
 })
 shoes.addEventListener('click', () => {
-  menu_products.innerHTML = '';
-  initStore(shoes.textContent);
+  element.forEach((event) => {
+    console.log(event.getAttribute('dataset'));
+    if(event.getAttribute('dataset') != 'shoes') {
+      console.log(event.getAttribute('dataset')+' == '+"shoes");
+      event.setAttribute('style', 'display: none');
+    } else {
+      event.setAttribute('style', 'display: flex');
+    }
+  })
 })
 shorts.addEventListener('click', () => {
-  menu_products.innerHTML = '';
-  initStore(shorts.textContent);
+  element.forEach((event) => {
+    console.log(event.getAttribute('dataset'));
+    if(event.getAttribute('dataset') != 'shorts') {
+      console.log(event.getAttribute('dataset')+' == '+"shorts");
+      event.setAttribute('style', 'display: none');
+    } else {
+      event.setAttribute('style', 'display: flex');
+    }
+  });
 })
 hats.addEventListener('click', () => {
-  menu_products.innerHTML = '';
-  initStore(hats.textContent);
-})
+  element.forEach((event) => {
+    console.log(event.getAttribute('dataset'));
+    if(event.getAttribute('dataset') != 'hats') {
+      console.log(event.getAttribute('dataset')+' == '+"hats");
+      event.setAttribute('style', 'display: none');
+    } else {
+      event.setAttribute('style', 'display: flex');
+    }
+  })})
 
 /*
 initShirts = () => {
