43762b9e6d811181929ce876fbd88f60a00fba98
Kevison Pascoal Mendes <kevison.mendes@gmail.com>
2022-08-27 10:50:25 -0300

    updated productSelected:zap:

--- js/product_selected.js
+++ js/product_selected.js
@@ -1,14 +1,71 @@
 const product = document.getElementById('product_selected');
+const masks = new Masks();
+const dataProduct = fetchProduct();
+
+function fetchProduct() {
+  const idProduct = localStorage.getItem("id");
+  const [ dataProduct ] = dataBase.filter(item => item.id === Number(idProduct));
+
+  return dataProduct;
+}
+
+function resetInTimer({ miliseconds, callback }) {
+  let timeout;
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    if (!!callback) {
+      callback();
+    }
+  }, miliseconds);
+}
 
 function initProduct() {
-  dataBase.forEach((event) => {
-    if(sessionStorage.getItem('id') == event.id) {
-      product.innerHTML = `
-        <div class="wrapper-selected">
-          <img src="${event.image}">
-        </div>
-      `
-    }
+
+  const money = masks.set(dataProduct.price).money();
+
+  product.innerHTML = `
+    <div class="wrapper-selected">
+      <img src="${dataProduct.image}">
+    </div>
+    <div class="about-product">
+      <div class="info">
+        <h2>${dataProduct.title}</h2>
+        <span class="money">${money}</span>
+      </div>
+      <button class="button-purchase-product">
+        <span>Comprar</span>
+        <img 
+          class="cart-icon" 
+          src="./assets/svgs/cart-icon.svg"
+        />
+      </button>
+    </div>
+  `;
+
+  const buttonPurchaseProduct = document.querySelector(".button-purchase-product");
+  const [ title ] = buttonPurchaseProduct.children;
+  title.innerText = title.innerText.toUpperCase();
+
+  buttonPurchaseProduct.addEventListener("click", () => {
+    buttonPurchaseProduct.classList.add("animation-click");
+    
+    const newDataProduct = {
+      ...dataProduct,
+      isPurchase: true,
+    };
+
+    const index = dataBase.findIndex(item => item.id === Number(newDataProduct.id));
+
+    dataBase[index] = newDataProduct;
+
+    console.log(dataBase[index]);
+
+    resetInTimer({
+      miliseconds: 260,
+      callback() {
+        buttonPurchaseProduct.classList.remove("animation-click");
+      }
+    })
   })
 }
 
